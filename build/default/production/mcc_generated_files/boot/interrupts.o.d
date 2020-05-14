dae759d33074bc614f3a51aecdf421ea9ce89e70
omarinov <orlinmirchev.marinov@asml.com>
2020-05-14 14:40:08 +0200

    initial project

---
+++ build/default/production/mcc_generated_files/boot/interrupts.o.d
@@ -0,0 +1,5 @@
+build/default/production/mcc_generated_files/boot/interrupts.o:  \
+ mcc_generated_files/boot/interrupts.S 
+build/default/production/mcc_generated_files/boot/interrupts.o:  \
+  \
+mcc_generated_files/boot/interrupts.S  \
